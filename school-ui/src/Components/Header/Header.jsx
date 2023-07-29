a594ee3d384149eb6147e486c8fbfb77f67c7e41
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-07-29 18:30:40 +0330

    Responsive Burger Menu

--- school-ui/src/Components/Header/Header.jsx
+++ school-ui/src/Components/Header/Header.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import Typewriter from "typewriter-effect";
 import { Link } from 'react-router-dom'
 import {BiSolidVideos , BiLogIn} from 'react-icons/bi'
 import {BsChevronDoubleDown} from 'react-icons/bs'
+import {FaBars} from 'react-icons/fa'
+import {AiOutlineClose} from  'react-icons/ai'
 function Header() {
+  const [isOpenHeader , setIsOpenHeader] = useState(false)
   const goToDown = () =>{
     window.scrollBy(0 , 500)
   }
+  const openHeader = () =>{
+    setIsOpenHeader(true)
+  }
+  const closeHeader = () =>{
+    setIsOpenHeader(false)
+  }
   return (
     <header>
+      {isOpenHeader && (
+      <div className="responsive-menu">
+        <AiOutlineClose onClick={closeHeader} className='close-icon'/>
+      <img src="./images/logo-blck.png" alt="school logo"  className='logo'/>
+        <ul className='responsive-list'>
+          <li>صفحه اصلی</li>
+          <li>پیش ثبت نام</li>
+          <li><BiSolidVideos /> ویدیو های آموزشی</li>
+          <li><BiLogIn /> ورود مدیر</li>
+        </ul>
+      </div>
+      )}
         <div className="top-menu">
+        <FaBars  className='responsive-icon' onClick={openHeader}/>
             <ul className="list-menu">
                <li className="list-menu-item"><Link to="/">صفحه اصلی</Link></li>
                <li className="list-menu-item"><Link to="/">پیش ثبت نام</Link></li>
