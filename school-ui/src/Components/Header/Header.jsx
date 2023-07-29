754777e02ee145dc9701d2230d947c3f0829f635
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-07-29 19:31:52 +0330

    Login JSX codes

--- school-ui/src/Components/Header/Header.jsx
+++ school-ui/src/Components/Header/Header.jsx
@@ -22,7 +22,7 @@ function Header() {
       {isOpenHeader && (
       <div className="responsive-menu">
         <AiOutlineClose onClick={closeHeader} className='close-icon'/>
-      <img src="./images/logo-blck.png" alt="school logo"  className='logo'/>
+      <img src="./images/logo-black.png" alt="school logo"  className='logo'/>
         <ul className='responsive-list'>
           <li>صفحه اصلی</li>
           <li>پیش ثبت نام</li>
