9be6ad248d46f15b8ec5c7275c5209930fcdae2d
amir25n <amir25nk@gmail.com>
2023-07-30 18:01:27 +0330

    feat(admin/creat): creat new admin

--- school-api/model/adminModel.js
+++ school-api/model/adminModel.js
@@ -1,20 +1,10 @@
 import { Schema, model } from "mongoose";
 
 const adminSchema = new Schema({
-    role: String,
-    fullName: {
-        type: String,
-        trim: true,
-    },
-    userName: {
-        unique: true,
-        type: String,
-        trim: true,
-    },
-    password: {
-        trim: true,
-        type: String,
-    },
+    role: { type: String, default: "admin" },
+    fullName: { type: String, trim: true, required: true },
+    userName: { unique: true, type: String, trim: true, required: true },
+    password: { trim: true, type: String, required: true },
 });
 
 export const Admin = model("Admin", adminSchema);
--- school-api/routes/adminRoutes.js
+++ school-api/routes/adminRoutes.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
 
 import { auth } from "../middlewares/auth.js";
+import { Admin } from "../model/adminModel.js";
 
 const router = Router();
 
 router.get("/", auth, (req, res) => {
     res.json({ message: "verify-ok" });
+});
+
+router.post("/signup", async (req, res) => {
+    try {
+        const { password, userName, fullname } = req.body;
+        const hash = await bcrypt.hash(password, 10);
+        await Admin.create({ password: hash, userName, fullname });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 export default router;
