4534d649a5e1f33c5b8eebd14aa514f713ee9e37
amir25n <amir25nk@gmail.com>
2023-07-30 10:10:35 +0330

    feat(admin/authenticated): authenticated middlewares

--- school-api/routes/publicRoutes.js
+++ school-api/routes/publicRoutes.js
@@ -9,12 +9,22 @@ router.get("/", (req, res) => {
     res.json({ message: `api is running` });
 });
 
-router.get("/login/admin", (req, res) => {
-    const { userName, password } = req.body;
+router.get("/login/admin", async (req, res) => {
+    try {
+        const { userName, password } = req.body;
 
-    const isMatch = Admin.findOne({ userName });
-    if (!isMatch && password != isMatch.password) {
-        res.json({ message: "not-valid" });
+        const isMatch = Admin.findOne({ userName });
+        if (!isMatch && password != isMatch.password) {
+            res.json({ message: "not-valid" });
+        }
+        jwt.sign(isMatch, process.env.JWT_SECRET, { expiresIn: "6d" }, (err, token) => {
+            if (err) return console.log(err);
+
+            res.json({
+                token,
+            });
+        });
+    } catch (error) {
+        console.log(error);
     }
-    jwt.sign(isMatch, process.env.JWT_SECRET);
 });
