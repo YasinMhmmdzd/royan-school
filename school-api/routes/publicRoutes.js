1c11707a87bab1b6e1d12745ec712a30a9766093
amir25n <amir25nk@gmail.com>
2023-07-30 09:27:53 +0330

    feat(admin/validate):validateion

--- school-api/routes/publicRoutes.js
+++ school-api/routes/publicRoutes.js
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+
+import { Admin } from "../model/adminModel";
 
 const router = Router();
 
@@ -6,6 +9,12 @@ router.get("/", (req, res) => {
     res.json({ message: `api is running` });
 });
 
-router.get("/login", (req, res) => {
-    res.json({ message: `api is running` });
+router.get("/login/admin", (req, res) => {
+    const { userName, password } = req.body;
+
+    const isMatch = Admin.findOne({ userName });
+    if (!isMatch && password != isMatch.password) {
+        res.json({ message: "not-valid" });
+    }
+    jwt.sign(isMatch, process.env.JWT_SECRET);
 });
