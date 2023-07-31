071a6f068fb99880356dfca47979be0db9d2c0ac
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-07-31 17:55:24 +0330

    change somethin in routing system

--- school-ui/src/Pages/Admin/Admin.jsx
+++ school-ui/src/Pages/Admin/Admin.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import "./Admin.css"
 import RightNav from '../../Components/AdminComponents/RightNav/RightNav'
-import pageRoutes from '../../Routes'
 function Admin() {
   return (
     <div className="admin-container">
-        <RightNav />
+        
     </div>
   )
 }
--- school-ui/src/Routes.js
+++ school-ui/src/Routes.js
@@ -2,13 +2,14 @@ import { Outlet } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Admin from "./Pages/Admin/Admin";
+import RightNav from "./Components/AdminComponents/RightNav/RightNav";
 import Students from "./Components/AdminComponents/Students/Students";
 let pageRoutes = [
     {path:"/" , element:<Home />},
     {path:"/login" , element:<Login />},
     {path:"/admin/*" , element:(
         <>
-    <Admin />
+    <RightNav />
     <Outlet />
         </>
     ) , children : [
