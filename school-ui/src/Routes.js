d28ac4c7f4f582338cb29adc8ff4d90b35751a08
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-07-31 10:08:31 +0330

    Add Admin to router system

--- school-ui/src/Routes.js
+++ school-ui/src/Routes.js
@@ -1,8 +1,9 @@
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
-
+import Admin from "./Pages/Admin/Admin";
 let pageRoutes = [
     {path:"/" , element:<Home />},
-    {path:"/login" , element:<Login />}
+    {path:"/login" , element:<Login />},
+    {path:"/admin" , element:<Admin />}
 ]
 export default pageRoutes
