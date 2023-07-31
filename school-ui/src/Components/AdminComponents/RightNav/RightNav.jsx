d14a33758adc873601d24bcbad5f9c0b4adc94e2
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-07-31 10:42:32 +0330

    Add RightNav Component to admin panel

---
+++ school-ui/src/Components/AdminComponents/RightNav/RightNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RightNav() {
+  return (
+    <div className="right-nav">
+        
+    </div>
+  )
+}
+
+export default RightNav
--- school-ui/src/Pages/Admin/Admin.jsx
+++ school-ui/src/Pages/Admin/Admin.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Admin.css"
+import pageRoutes from '../../Routes'
+import RightNav from '../../Components/AdminComponents/RightNav/RightNav'
 function Admin() {
   return (
     <div className="admin-container">
-        
+        <RightNav />
     </div>
   )
 }
