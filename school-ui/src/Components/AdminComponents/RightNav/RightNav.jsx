8f1815791cc56ad0478e45c7d02ad112deac9ce8
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-01 15:18:47 +0330

    Add home page

--- school-ui/src/Components/AdminComponents/RightNav/RightNav.jsx
+++ school-ui/src/Components/AdminComponents/RightNav/RightNav.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import "./RightNav.css"
 import Cookies from 'js-cookie'
 import { Link, Navigate } from 'react-router-dom'
-import {FaUsers} from 'react-icons/fa'
+import {FaUsers , FaHome} from 'react-icons/fa'
 import {BsFillKeyFill} from 'react-icons/bs'
 import {AiFillSetting} from 'react-icons/ai'
 import {FiLogOut} from 'react-icons/fi'
@@ -22,9 +22,10 @@ function RightNav() {
       <div className="top-right-nav">
         <img src="../images/logo-black.png" alt="لوگوی دبیرستان رویان" />
         <ul className="list-menu-admin">
-          <li className="list-menu-item-admin"><Link to="/" className='list-link'><FaUsers className='list-icon'/> دانش آموزان</Link></li>
-          <li className="list-menu-item-admin"><Link to="/" className='list-link'><BsFillKeyFill className='list-icon'/>مدیران</Link></li>
-          <li className="list-menu-item-admin"><Link to="/" className='list-link'><AiFillSetting className='list-icon'/> تنظیمات</Link></li>
+        <li className="list-menu-item-admin"><Link to="/" className='list-link'><FaHome className='list-icon'/> صفحه اصلی</Link></li>
+          <li className="list-menu-item-admin"><Link to="/admin/students" className='list-link'><FaUsers className='list-icon'/> دانش آموزان</Link></li>
+          <li className="list-menu-item-admin"><Link to="/admin/admins" className='list-link'><BsFillKeyFill className='list-icon'/>مدیران</Link></li>
+          <li className="list-menu-item-admin"><Link to="/admin/setting" className='list-link'><AiFillSetting className='list-icon'/> تنظیمات</Link></li>
         </ul>
       </div>
       <div className="bottom-right-nav">
