555fac7abf81009c3d49fa61ac6c2f7d9428766d
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-01 15:57:04 +0330

    Add Students component JSX and styles

--- school-ui/src/Components/AdminComponents/RightNav/RightNav.jsx
+++ school-ui/src/Components/AdminComponents/RightNav/RightNav.jsx
@@ -4,8 +4,8 @@ import Cookies from 'js-cookie'
 import { Link, Navigate } from 'react-router-dom'
 import {FaUsers , FaHome} from 'react-icons/fa'
 import {BsFillKeyFill} from 'react-icons/bs'
-import {AiFillSetting} from 'react-icons/ai'
 import {FiLogOut} from 'react-icons/fi'
+import {BiSolidVideos} from 'react-icons/bi'
 function RightNav() {
   const [navigateOk , setNavigateOk] = useState(false)
   const logOutAdmin = () =>{
@@ -25,7 +25,7 @@ function RightNav() {
         <li className="list-menu-item-admin"><Link to="/" className='list-link'><FaHome className='list-icon'/> صفحه اصلی</Link></li>
           <li className="list-menu-item-admin"><Link to="/admin/students" className='list-link'><FaUsers className='list-icon'/> دانش آموزان</Link></li>
           <li className="list-menu-item-admin"><Link to="/admin/admins" className='list-link'><BsFillKeyFill className='list-icon'/>مدیران</Link></li>
-          <li className="list-menu-item-admin"><Link to="/admin/setting" className='list-link'><AiFillSetting className='list-icon'/> تنظیمات</Link></li>
+          <li className="list-menu-item-admin"><Link to="/admin/courses" className='list-link'><BiSolidVideos className='list-icon'/> دوره های آموزشی</Link></li>
         </ul>
       </div>
       <div className="bottom-right-nav">
