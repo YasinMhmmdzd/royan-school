01bdb8f8b78d6d818198e7be38f16d44e4714efc
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-01 18:31:12 +0330

    Add (add-admin) component to router

--- school-ui/src/Routes.js
+++ school-ui/src/Routes.js
@@ -1,8 +1,9 @@
-import { Outlet } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Admin from "./Pages/Admin/Admin";
 import Students from "./Components/AdminComponents/Students/Students";
+import Admins from "./Components/AdminComponents/Admins/Admins";
+import AddNewAdmin from "./Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin";
 let pageRoutes = [
     {path:"/" , element:<Home />},
     {path:"/login" , element:<Login />},
@@ -12,7 +13,9 @@ let pageRoutes = [
         </>
     ) , children : [
         {path:"students" , element:<Students />},
-        {path:"admins" , element:<Admins />}
+        {path:"admins" , element:<Admins /> , children:[
+            {path:"add" , element:<AddNewAdmin />}
+        ]}
     ]}
 ]
 export default pageRoutes
