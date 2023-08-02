72a8c3b48782d61b5b770ebf56d4761dd3b0c6b9
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-02 12:50:21 +0330

    AdminLists JSX and styles

--- school-ui/src/Components/AdminComponents/Admins/AdminLists/AdminLists.jsx
+++ school-ui/src/Components/AdminComponents/Admins/AdminLists/AdminLists.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import "./AdminLists.css"
+import {AiFillDelete , AiFillEdit} from 'react-icons/ai'
 function AdminLists() {
   return (
-    <div>AdminLists</div>
+    <div className="admin-lists-container">
+      <h2>لیست مدیران</h2>
+      <table>
+        <thead>
+          <tr>
+          <th>نام و نام خانوادگی</th>
+          <th>نام کاربری</th>
+          <th>عملیات</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>یاسین محمدزاده</td>
+            <td dir='ltr'>@mmd</td>
+            <td>
+              <AiFillDelete className='opreation-icon error-icon'/>
+              <AiFillEdit className='opreation-icon edit-icon'/>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
