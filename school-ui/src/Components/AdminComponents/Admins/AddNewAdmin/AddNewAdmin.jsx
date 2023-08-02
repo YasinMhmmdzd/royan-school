4555817072a662806d279f3474a2f8fa31bb4c4d
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-02 12:09:59 +0330

    Add SubmitHandler and states

--- school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
+++ school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./AddNewAdmin.css"
 import { useContext } from 'react'
 import adminContext from '../../../../Contexts/AdminContexts'
 function AddNewAdmin() {
   const adminInfos = useContext(adminContext)
+  const [newAdminFullName ,setNewAdminFullName] = useState('')
+  const [newAdminName , setNewAdminName] = useState('')
+  const [newAdminPassword , setNewAdminPassword] = useState('')
+  const newAdminHandler = (e)=>{
+    e.preventDefault()
+  }
   return (
     <div className='add-admin-container'>
       <h2>اضافه کردن ادمین</h2>
-      <form>
+      <form onSubmit={newAdminHandler}>
         <input type="text" className='admin-signup-input' placeholder='نام و نام خانوادگی'/>
         <input type="text" className='admin-signup-input' placeholder='نام کاربری (انگلیسی باشد)'/>
         <input type="password" className='admin-signup-input' placeholder='رمزعبور'/>
