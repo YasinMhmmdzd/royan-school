a8f27e7bc3c7b700b203f3e1a02f129a566c2cc5
amir25n <amir25nk@gmail.com>
2023-08-02 03:34:58 +0330

    feat(user/login): add middleware user login

--- school-api/routes/publicRoutes.js
+++ school-api/routes/publicRoutes.js
@@ -35,4 +35,34 @@ router.post("/login/admin", async (req, res) => {
     }
 });
 
+router.post("/login/user", async (req, res) => {
+    try {
+        const { uniqueCode, phoneNumber } = req.body;
+        const isMatch = await findOne({ uniqueCode });
+
+        if (!isMatch || phoneNumber == isMatch.phoneNumber) {
+            return res.json({ message: "not-valid " });
+        }
+        const userPayload = {
+            id: isMatch._id,
+            fullName: isMatch.fullName,
+            uniqueCode: isMatch.uniqueCode,
+            motherNumber: isMatch.motherNumber,
+            fatherNumber: isMatch.fatherNumber,
+            Grade: isMatch.Grade,
+            studyFeild: isMatch.studyFeild,
+        };
+        jwt.sign(userPayload, process.env.JWT_SECRET, { expiresIn: "6d" }, (err, token) => {
+            if (err) return console.log(err);
+
+            res.json({
+                token,
+                message: "success",
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 export default router;
