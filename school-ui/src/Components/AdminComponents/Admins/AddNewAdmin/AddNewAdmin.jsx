64e973e5e1d2a4637f484ab07b9fca8a9c225553
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-02 12:04:05 +0330

    AddAdmin Component JSX and styles

--- school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
+++ school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import "./AddNewAdmin.css"
+import { useContext } from 'react'
+import adminContext from '../../../../Contexts/AdminContexts'
 function AddNewAdmin() {
+  const adminInfos = useContext(adminContext)
   return (
-    <div>AddNewAdmin</div>
+    <div className='add-admin-container'>
+      <h2>اضافه کردن ادمین</h2>
+      <form>
+        <input type="text" className='admin-signup-input' placeholder='نام و نام خانوادگی'/>
+        <input type="text" className='admin-signup-input' placeholder='نام کاربری (انگلیسی باشد)'/>
+        <input type="password" className='admin-signup-input' placeholder='رمزعبور'/>
+        <button className='signup-btn'>اضافه کردن کاربر</button>
+      </form>
+    </div>
   )
 }
 
