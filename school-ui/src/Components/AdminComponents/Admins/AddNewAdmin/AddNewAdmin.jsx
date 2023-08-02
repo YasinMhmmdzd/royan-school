d56e86f2ec72db541cb227f5cbda161f88d6b15e
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-02 18:10:57 +0330

    fix add admin status

--- school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
+++ school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
@@ -6,8 +6,10 @@ function AddNewAdmin() {
   const [newAdminFullName ,setNewAdminFullName] = useState('')
   const [newAdminName , setNewAdminName] = useState('')
   const [newAdminPassword , setNewAdminPassword] = useState('')
+  const [fetchStatus , setFetchStatus] = useState("")
   const newAdminHandler = (e)=>{
     e.preventDefault()
+    setFetchStatus("pending")
     axios.post("https://school-node.iran.liara.run/admin/signup" , {
       fullName : newAdminFullName ,
       userName : newAdminName ,
@@ -17,7 +19,10 @@ function AddNewAdmin() {
         token : Cookies.get("adminToken")
       }
     })
-    .then(res => console.log(res))
+    .then((res) =>{
+      console.log(res);
+      setFetchStatus(res.data.message)
+    })
   }
   return (
     <div className='add-admin-container'>
@@ -27,6 +32,18 @@ function AddNewAdmin() {
         <input type="text" className='admin-signup-input' onChange={(e) => setNewAdminName(e.target.value)} placeholder='نام کاربری (انگلیسی باشد)'/>
         <input type="password" className='admin-signup-input' onChange={(e) => setNewAdminPassword(e.target.value)} placeholder='رمزعبور'/>
         <button className='signup-btn'>اضافه کردن کاربر</button>
+        {fetchStatus === "pending" && (
+          <p className="loading">درحال پردازش...</p>
+        )}
+        {fetchStatus === "admin-not-valid" && (
+          <p className="err">شما دسترسی اضافه کردن مدیر را ندارید</p>
+        )}
+        {fetchStatus === "admin-used" && (
+          <p className="err">این نام کاربری مورد استفاده است</p>
+        )}
+        {fetchStatus === "admin-created" && (
+          <p className="success" style={{animation:'none'}}>کاربر جدید ساخته شد</p>
+        )}
       </form>
     </div>
   )
