4ad10b862c8d1c27fe1b94b12e3d257fd00d4bbd
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-02 12:23:00 +0330

    add api to project

--- school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
+++ school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import "./AddNewAdmin.css"
 import { useContext } from 'react'
 import adminContext from '../../../../Contexts/AdminContexts'
+import axios from 'axios'
 function AddNewAdmin() {
   const adminInfos = useContext(adminContext)
   const [newAdminFullName ,setNewAdminFullName] = useState('')
@@ -9,14 +10,20 @@ function AddNewAdmin() {
   const [newAdminPassword , setNewAdminPassword] = useState('')
   const newAdminHandler = (e)=>{
     e.preventDefault()
+    axios.post("https://school-node.iran.liara.run/admin/signup" , {
+      fullName : newAdminFullName ,
+      userName : newAdminName ,
+      password : newAdminPassword
+    })
+    .then(res => console.log(res))
   }
   return (
     <div className='add-admin-container'>
       <h2>اضافه کردن ادمین</h2>
       <form onSubmit={newAdminHandler}>
-        <input type="text" className='admin-signup-input' placeholder='نام و نام خانوادگی'/>
-        <input type="text" className='admin-signup-input' placeholder='نام کاربری (انگلیسی باشد)'/>
-        <input type="password" className='admin-signup-input' placeholder='رمزعبور'/>
+        <input type="text" className='admin-signup-input' onChange={(e) => setNewAdminFullName(e.target.value)} placeholder='نام و نام خانوادگی'/>
+        <input type="text" className='admin-signup-input' onChange={(e) => setNewAdminName(e.target.value)} placeholder='نام کاربری (انگلیسی باشد)'/>
+        <input type="password" className='admin-signup-input' onChange={(e) => setNewAdminPassword(e.target.value)} placeholder='رمزعبور'/>
         <button className='signup-btn'>اضافه کردن کاربر</button>
       </form>
     </div>
