eb30ef07551bccce121598756dea01e7bee06713
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-02 23:18:19 +0330

    fix re renderig

--- school-ui/src/Components/AdminComponents/Admins/AdminLists/AdminLists.jsx
+++ school-ui/src/Components/AdminComponents/Admins/AdminLists/AdminLists.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import "./AdminLists.css"
-import {AiFillDelete , AiFillEdit} from 'react-icons/ai'
+import {AiFillDelete} from 'react-icons/ai'
 import Cookies from 'js-cookie'
 import axios from 'axios'
+import { Navigate } from 'react-router-dom'
+
+
 function AdminLists() {
+
+
   const [allAdmins , setAllAdmins] = useState([])
-  const deleteUser = (userName) => {
-    axios.delete(`https://school-node.iran.liara.run/admin/delete/${userName}` , {
-      headers : {
-        token : Cookies.get("adminToken")
-      }
-    }).then(res => console.log(res))
-  }
+  const [fetchDeleteStatus , setFetchDeleteStatus] = useState("")
+
+
   useEffect(()=> {
     axios.get("https://school-node.iran.liara.run/admin/getAll" , {
       headers :{
@@ -22,10 +23,44 @@ function AdminLists() {
       console.log(res);
       setAllAdmins(res.data)
     })
-  } , [])
+  } , [fetchDeleteStatus])
+
+
+  const deleteUser = (userName) => {
+    axios.delete(`https://school-node.iran.liara.run/admin/delete/${userName}` , {
+      headers : {
+        token : Cookies.get("adminToken")
+      }
+    }).then(
+      (res) => {
+        setFetchDeleteStatus(res.data.message)
+      }
+    )
+  }
+
+
   return (
+
+
     <div className="admin-lists-container">
       <h2>لیست مدیران</h2>
+      {
+        fetchDeleteStatus === "delete-ok" && (
+          <>
+          <p className="success" style={{animation:'none' , textAlign:'center'}}>
+            کاربر حذف شد
+          </p>
+          <Navigate to="/admin/admins/list"/>
+          </>
+        )
+      }
+      {
+        fetchDeleteStatus === "admin-not-access" && (
+          <p className="err">
+            شما دسترسی حذف کاربر را ندارید
+          </p>
+        )
+      }
       <table>
         <thead>
           <tr>
