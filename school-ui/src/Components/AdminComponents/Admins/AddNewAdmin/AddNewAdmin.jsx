7318c59b3f99dac20ed737e2854944b1c1b48be5
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-03 15:09:17 +0330

    add admin validation

--- school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
+++ school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
@@ -7,29 +7,44 @@ function AddNewAdmin() {
   const [newAdminName , setNewAdminName] = useState('')
   const [newAdminPassword , setNewAdminPassword] = useState('')
   const [fetchStatus , setFetchStatus] = useState("")
+  const [isSubmitted , setIsSubmitted] = useState(false)
   const newAdminHandler = (e)=>{
     e.preventDefault()
-    setFetchStatus("pending")
-    axios.post("https://school-node.iran.liara.run/admin/signup" , {
-      fullName : newAdminFullName ,
-      userName : newAdminName ,
-      password : newAdminPassword
-    } ,{
-      headers:{
-        token : Cookies.get("adminToken")
-      }
-    })
-    .then((res) =>{
-      setFetchStatus(res.data.message)
-    })
+    setIsSubmitted(true)
+    if(newAdminFullName.length > 3 && newAdminName.length > 2 && newAdminPassword.length > 8){
+
+      setFetchStatus("pending")
+      axios.post("https://school-node.iran.liara.run/admin/signup" , {
+        fullName : newAdminFullName ,
+        userName : newAdminName ,
+        password : newAdminPassword
+      } ,{
+        headers:{
+          token : Cookies.get("adminToken")
+        }
+      })
+      .then((res) =>{
+        setFetchStatus(res.data.message)
+      })
+    
+    }
   }
   return (
     <div className='add-admin-container'>
       <h2>اضافه کردن ادمین</h2>
       <form onSubmit={newAdminHandler}>
         <input type="text" className='admin-signup-input' onChange={(e) => setNewAdminFullName(e.target.value)} placeholder='نام و نام خانوادگی'/>
+        {(isSubmitted && newAdminFullName.length < 2) && (
+          <p className="err">نام باید بیشتر از 2 کاراکتر باشد</p>
+        )}
         <input type="text" className='admin-signup-input' onChange={(e) => setNewAdminName(e.target.value)} placeholder='نام کاربری (انگلیسی باشد)'/>
-        <input type="password" className='admin-signup-input' onChange={(e) => setNewAdminPassword(e.target.value)} placeholder='رمزعبور'/>
+        {(isSubmitted && newAdminName.length < 2) && (
+          <p className="err">نام کاربری باید بیشتر از ۲ کاراکتر باشد</p>
+        )}
+        <input type="password" className='admin-signup-input' onChange={(e) => setNewAdminPassword(e.target.value)} placeholder='رمز عبور‌(بیش از ۸ کاراکتر)'/>
+        {(isSubmitted && newAdminPassword.length < 8) && (
+          <p className="err">رمز عبور باید بیش از ۸ کاراکتر باشد</p>
+        )}
         <button className='signup-btn'>اضافه کردن کاربر</button>
         {fetchStatus === "pending" && (
           <p className="loading">درحال پردازش...</p>
