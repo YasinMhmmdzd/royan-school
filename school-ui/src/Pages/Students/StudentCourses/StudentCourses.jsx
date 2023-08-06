5ee3643b6a1f80cbbc14bd593a8c8081492fe9bb
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-06 20:53:52 +0330

    add courses component

--- school-ui/src/Pages/Students/StudentCourses/StudentCourses.jsx
+++ school-ui/src/Pages/Students/StudentCourses/StudentCourses.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import React, { useEffect, useState } from 'react'
+import { Navigate } from 'react-router-dom'
 
 function StudentCourses() {
+
+  const [studentInfos , setStudentInfos] = useState([])
+  const [verifyStatus , setVerifyStatus] = useState('')
+
+
+  async function fetchData(){
+    await axios.get("https://school-node.iran.liara.run/user" , {
+      headers : {
+        token : Cookies.get("studentToken")
+      }
+    }).then((res) => {
+        setVerifyStatus(res.data.message)
+        setStudentInfos(res.data.user)
+    })
+  }
+
+  useEffect(()=>{
+    fetchData()
+  })
+
+
   return (
+    <>
+
+
+    {
+      verifyStatus === "token-error" && (
+        <Navigate to="/login" />
+      )
+    }
+
+    {verifyStatus === "verify-ok" && (
     <div>StudentCourses</div>
+    )}
+
+    
+    </>
   )
 }
 
