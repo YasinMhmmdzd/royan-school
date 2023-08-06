a935eeb02b4bee2129ae81867867a7b52437755c
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-06 22:03:05 +0330

    AddStudent form validation

--- school-ui/src/Components/AdminComponents/Students/AddNewStudent/AddNewStudent.jsx
+++ school-ui/src/Components/AdminComponents/Students/AddNewStudent/AddNewStudent.jsx
@@ -13,28 +13,31 @@ function AddNewStudent() {
   const [studentGrade , setStudentGrade] = useState('')
   const [studentField , setStudentField] = useState("")
   const [fetchStatus , setFetchStatus] = useState('')
+  const [submitted , setSubmitted] = useState(false)
 
   const newStudentHandler = (e) => {
     e.preventDefault()
-    setFetchStatus('pending')
-    axios.post("https://school-node.iran.liara.run/admin/user/signup" , {
-      fullName : studentFullName ,
-      phoneNumber : studentPhoneNumber , 
-      uniqueCode : studentNationalCode ,
-      motherNumber : studentMohterPhoneNumber ,
-      fatherNumber : studentFatherNumber , 
-      Grade : studentGrade , 
-      studyField : studentField
-    } ,{
-      headers : {
-        token : Cookies.get("adminToken")
-      }
-    }).then(
-      (res) => {
-        console.log(res);
-        setFetchStatus(res.data.message)
-      }
-    )
+    setSubmitted(true)
+    if(studentNationalCode.length === 10 && studentPhoneNumber.length === 11 && studentMohterPhoneNumber.length === 11 && studentFatherNumber.length === 11 && studentField !== "" && studentGrade !==""){
+      setFetchStatus('pending')
+      axios.post("https://school-node.iran.liara.run/admin/user/signup" , {
+        fullName : studentFullName ,
+        phoneNumber : studentPhoneNumber , 
+        uniqueCode : studentNationalCode ,
+        motherNumber : studentMohterPhoneNumber ,
+        fatherNumber : studentFatherNumber , 
+        Grade : studentGrade , 
+        studyField : studentField
+      } ,{
+        headers : {
+          token : Cookies.get("adminToken")
+        }
+      }).then(
+        (res) => {
+          setFetchStatus(res.data.message)
+        }
+      )
+    }
   }
 
 
@@ -46,9 +49,25 @@ function AddNewStudent() {
       <form onSubmit={newStudentHandler}>
       <input type="text" className='student-signup-input' onChange={(e) => setStudentFullName(e.target.value)} placeholder='نام و نام خانوادگی'/>
       <input type="text" className='student-signup-input' onChange={(e) => setStudentPhoneNumber(e.target.value)} placeholder='شماره تلفن همراه'/>
+      {(submitted && studentPhoneNumber.length !== 11) && (
+        <p className="err">شماره موبایل باید ۱۱ رقم باشد</p>
+      )}
       <input type="text" className='student-signup-input' onChange={(e) => setStudentNationalCode(e.target.value)} placeholder='شماره ملی'/>
+      {(submitted && studentNationalCode.length !== 10) && (
+        <p className="err">شماره ملی باید ۱۰ رقم باشد</p>
+      )}
       <input type="text" className='student-signup-input' onChange={(e) => setStudentMotherPhoneNumber(e.target.value)} placeholder='شماره همراه مادر'/>
+      {
+        (submitted && studentMohterPhoneNumber.length !== 11) && (
+          <p className="err">شماره همراه باید ۱۰ رقم باشد</p>
+        )
+      }
       <input type="text" className='student-signup-input' onChange={(e) => setStudentFatherNumber(e.target.value)} placeholder='شماره همراه پدر'/>
+      {
+        (submitted && studentFatherNumber.length !== 11) && (
+          <p className="err">شماره همراه باید ۱۰ رقم باشد</p>
+        )
+      }
       پایه تحصیلی :
       <select className='student-signup-select' onChange={(e) => setStudentGrade(e.target.value)}>
         <option value="">−−−−</option>
@@ -56,12 +75,22 @@ function AddNewStudent() {
         <option value="2">یازدهم</option>
         <option value="3">دوازدهم</option>
       </select>
+      {
+        (submitted && studentGrade === '') && (
+          <p className="err">پایه تحصیلی را وارد کنید</p>
+        )
+      }
       <br />
       رشته تحصیلی :‌
       <select className='student-signup-select' onChange={(e) => setStudentField(e.target.value)}>
         <option value="1">تجربی</option>
         <option value="2">ریاضی</option>
       </select>
+      {
+        (submitted && studentField === '') && (
+          <p className="err">رشته را وارد کنید</p>
+        )
+      }
       <button className='signup-btn'>اضافه کردن کاربر</button>
       {fetchStatus === "pending" && (
         <p className="loading"> درحال پردازش...</p>
