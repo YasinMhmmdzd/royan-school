b24eee883991bc8891b07c78a53bcb3eb0eb546a
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-09 17:26:25 +0330

    Create Courses Components and CoursesHeader Componetn

--- school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
+++ school-ui/src/Components/AdminComponents/Admins/AddNewAdmin/AddNewAdmin.jsx
@@ -3,11 +3,15 @@ import "./AddNewAdmin.css"
 import axios from 'axios'
 import Cookies from 'js-cookie'
 function AddNewAdmin() {
+
+
   const [newAdminFullName ,setNewAdminFullName] = useState('')
   const [newAdminName , setNewAdminName] = useState('')
   const [newAdminPassword , setNewAdminPassword] = useState('')
   const [fetchStatus , setFetchStatus] = useState("")
   const [isSubmitted , setIsSubmitted] = useState(false)
+
+
   const newAdminHandler = (e)=>{
     e.preventDefault()
     setIsSubmitted(true)
@@ -30,36 +34,56 @@ function AddNewAdmin() {
     }
   }
   return (
+
+
     <div className='add-admin-container'>
       <h2>اضافه کردن ادمین</h2>
       <form onSubmit={newAdminHandler}>
+
+        
         <input type="text" className='admin-signup-input' onChange={(e) => setNewAdminFullName(e.target.value)} placeholder='نام و نام خانوادگی'/>
         {(isSubmitted && newAdminFullName.length < 2) && (
           <p className="err">نام باید بیشتر از 2 کاراکتر باشد</p>
         )}
+
+
         <input type="text" className='admin-signup-input' onChange={(e) => setNewAdminName(e.target.value)} placeholder='نام کاربری (انگلیسی باشد)'/>
         {(isSubmitted && newAdminName.length < 2) && (
           <p className="err">نام کاربری باید بیشتر از ۲ کاراکتر باشد</p>
         )}
+
+
         <input type="password" className='admin-signup-input' onChange={(e) => setNewAdminPassword(e.target.value)} placeholder='رمز عبور‌(بیش از ۸ کاراکتر)'/>
         {(isSubmitted && newAdminPassword.length <= 7) && (
           <p className="err">رمز عبور باید بیش از ۸ کاراکتر باشد</p>
         )}
+
+
         <button className='signup-btn' disabled={fetchStatus === "pending" ? true : false}>اضافه کردن کاربر</button>
         {fetchStatus === "pending" && (
           <p className="loading">درحال پردازش...</p>
         )}
+
+
         {fetchStatus === "admin-not-valid" && (
           <p className="err">شما دسترسی اضافه کردن مدیر را ندارید</p>
         )}
+
+
         {fetchStatus === "admin-used" && (
           <p className="err">این نام کاربری مورد استفاده است</p>
         )}
+
+
         {fetchStatus === "admin-created" && (
           <p className="success" style={{animation:'none'}}>کاربر جدید ساخته شد</p>
         )}
+
+
       </form>
     </div>
+
+
   )
 }
 
---
+++ school-ui/src/Components/CoursesComponents/CoursesHeader/CoursesHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./CoursesHeader.css"
+function CoursesHeader() {
+  return (
+    <div>CoursesHeader</div>
+  )
+}
+
+export default CoursesHeader
