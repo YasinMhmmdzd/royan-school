64a341c9c28f6e2c445771d26aa0ce8501642d52
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-09 19:04:14 +0330

    Add css and logic

--- school-ui/src/Components/CoursesComponents/CoursesHeader/CoursesHeader.jsx
+++ school-ui/src/Components/CoursesComponents/CoursesHeader/CoursesHeader.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./CoursesHeader.css"
 import { Link } from 'react-router-dom'
-import { AiOutlineArrowLeft } from 'react-icons/ai' 
+import { AiOutlineArrowLeft , AiOutlineUser } from 'react-icons/ai' 
+import {BiLogOut} from 'react-icons/bi'
 
 function CoursesHeader() {
+
+    const [isOpenInfoBox , setIsOpenInfoBox] = useState(false)
+
   return (
 
     <div className="courses-header">
 
-        <div className="student-info-box">
-            <p>یاسین محمدزاده</p>
+        <div className="student-info-box" onClick={() => setIsOpenInfoBox(prev => !prev)}>
+            <p>
+                <AiOutlineUser />
+                یاسین محمدزاده
+            </p>
+            {isOpenInfoBox && (
+
+            <div className="student-info-sub-box">
+                <Link to="/">
+                    <BiLogOut />
+                    خروج
+                    </Link>
+            </div>
+
+            )}
         </div>
 
         <div className="go-to-home">
