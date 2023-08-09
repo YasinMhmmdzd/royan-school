3ba9b0097d08e00ab696cf16df57c0183e3c27de
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-09 20:49:34 +0330

    Courses header logic

--- school-ui/src/Components/CoursesComponents/CoursesHeader/CoursesHeader.jsx
+++ school-ui/src/Components/CoursesComponents/CoursesHeader/CoursesHeader.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from 'react'
 import "./CoursesHeader.css"
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import { AiOutlineArrowLeft , AiOutlineUser } from 'react-icons/ai' 
 import {BiLogOut} from 'react-icons/bi'
+import Cookies from 'js-cookie'
 
 function CoursesHeader() {
 
     const [isOpenInfoBox , setIsOpenInfoBox] = useState(false)
+    const [isNavigate , setIsNavigate] = useState(false)
+
+    const logOutStudent = () =>{
+        Cookies.remove("studentToken")
+        setIsNavigate(true)
+        console.log("clicked");
+    }
 
   return (
 
+    <>
+    {
+        isNavigate && (
+            <Navigate to="/" />
+        )
+    }
+    
     <div className="courses-header">
 
         <div className="student-info-box" onClick={() => setIsOpenInfoBox(prev => !prev)}>
@@ -20,10 +35,10 @@ function CoursesHeader() {
             {isOpenInfoBox && (
 
             <div className="student-info-sub-box">
-                <Link to="/">
+                <button onClick={() => logOutStudent}>
                     <BiLogOut />
                     خروج
-                    </Link>
+                </button>
             </div>
 
             )}
@@ -37,6 +52,9 @@ function CoursesHeader() {
 
         </div>
     </div>
+
+    </>
+
 
   )
 }
