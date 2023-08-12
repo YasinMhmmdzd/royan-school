ae45b063a4bd867cb4979f007e474e8654e93da7
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-08-12 18:05:09 +0330

    Add go back link for staff component

--- school-ui/src/Pages/StaffLists/StaffLists.jsx
+++ school-ui/src/Pages/StaffLists/StaffLists.jsx
@@ -2,14 +2,25 @@ import React from 'react'
 import staffDatas from './StaffDatas'
 import StaffItem from './StaffItem/StaffItem'
 import Footer from '../../Components/Footer/Footer'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 import "./StaffLists.css"
+import { Link } from 'react-router-dom'
 function StaffLists() {
   return (
-    <>    
+    <> 
+    <div className="staff-lists">
+      <div className="staff-header">
+        <Link to="/">
+      <AiOutlineArrowLeft className='staff-exit-icon'/>
+        </Link>
+      </div>
+
+
     <div className="staff-container">
         {staffDatas.map(staff => (
             <StaffItem {...staff}/>
         ))}
+    </div>
     </div>
       <Footer />
     </>
