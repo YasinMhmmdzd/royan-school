7c7d5b18b9c5ca5d7b5db3ac85433a3b1e035c6b
amir25n <amir25nk@gmail.com>
2023-08-14 10:57:59 +0330

    feat(admin/video): add base video

--- school-api/routes/userRoutes.js
+++ school-api/routes/userRoutes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import { authUser } from "../middlewares/auth.js";
+// import multer from "multer";
+// import multerS3 from "multer-s3";
+// import AWS from "aws-sdk";
 
 const router = Router();
 
