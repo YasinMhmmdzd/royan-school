011b49975e3188df8111b8b6e86667a306cbbfd6
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-08-19 19:19:10 +0330

    Add login JSX codes

--- school-ui/src/Components/AdminComponents/AdminCourses/AdminCourses.jsx
+++ school-ui/src/Components/AdminComponents/AdminCourses/AdminCourses.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import {BsFillPencilFill} from "react-icons/bs"
 
+import "./AdminCourses.css"
+import { Link } from "react-router-dom";
 export default function AdminCourses(){
     return(
-        <div>Hello</div>
+        <div className="admin-courses-container left-part">
+
+        <div className="admin-courses-item">
+            <Link>
+        <AiOutlineCloudUpload />
+            آپلود ویدیو
+            
+            </Link>
+        </div>
+        <div className="admin-courses-item">
+            <Link>
+        <BsFillPencilFill />
+        مشاهده ویدیو ها
+            
+            </Link>
+        </div>
+
+        </div>
     )
 }
