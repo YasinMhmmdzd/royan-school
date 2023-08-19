e744d19078eb2dabb0f9c1172865062aa1f3c09e
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-08-19 21:57:32 +0330

    feat(Admin/Courses) : Upload Course Logic

--- school-ui/src/Components/AdminComponents/AdminCourses/AdminCourseUpload/AdminCourseUpload.jsx
+++ school-ui/src/Components/AdminComponents/AdminCourses/AdminCourseUpload/AdminCourseUpload.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import "./AdminCourseUpload.css"
 function AdminCourseUpload() {
+
+    const [appendedFileName , setAppendedFileName] = useState("")
+
+    const uploadFileHandler = (event)=>{
+
+        const videoData = new FormData()
+
+        videoData.append('file' , event.target.files[0])
+
+    }
+
   return (
     <div className='admin-courses-upload left-part'>
 
         <h2>فایل دوره را آپلود کنید</h2>
-        <form className='admin-course-upload-form'>
+        <form className='admin-course-upload-form' onSubmit={uploadFileHandler}>
 
             <label className='upload-video-lable'>
                 
-            <input type="file" className='upload-video-input' style={{color:'blue'}}/>
+            <input type="file" className='upload-video-input' style={{color:'blue'}} onChange={(event) => setAppendedFileName(event.target.files[0].name)}/>
             <AiOutlineCloudUpload />
-            فایل را آپلود کنید
+            آپلود فایل
             </label>
+
+            {appendedFileName}
+            <button className='upload-video-button'>ارسال فایل</button>
         </form>
 
     </div>
