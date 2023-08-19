546249f66c21183c9823dfce29301d460d98fbab
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-08-19 21:33:21 +0330

    feat(Amin/Courses/Upload) : add upload JSX codes

--- school-ui/src/Components/AdminComponents/AdminCourses/AdminCourseUpload/AdminCourseUpload.jsx
+++ school-ui/src/Components/AdminComponents/AdminCourses/AdminCourseUpload/AdminCourseUpload.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import "./AdminCourseUpload.css"
 function AdminCourseUpload() {
   return (
-    <div>AdminCourseUpload</div>
+    <div className='admin-courses-upload left-part'>
+
+        <h2>فایل دوره را آپلود کنید</h2>
+        <form className='admin-course-upload-form'>
+
+            <label className='upload-video-lable'>
+                
+            <input type="file" className='upload-video-input' style={{color:'blue'}}/>
+            <AiOutlineCloudUpload />
+            فایل را آپلود کنید
+            </label>
+        </form>
+
+    </div>
   )
 }
 
