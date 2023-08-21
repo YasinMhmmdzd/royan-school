8665891ed3f9c6cdc1e553f2c0b7f99a17dce809
amir25n <amir25nk@gmail.com>
2023-08-21 12:27:45 +0330

    feat(aws/delete): delete object video

--- school-api/utils/s3.js
+++ school-api/utils/s3.js
@@ -28,8 +28,22 @@ export const s3Download = async (name) => {
             secretAccessKey: process.env.LIARA_SECRET_KEY,
             region: "default",
         });
-        const params = { Bucket: process.env.LIARA_BUCKET_NAME, Key: `${name}.mp4`, Expires: 60 };
-        return await s3.getSignedUrlPromise("getObject", params);
+        return await s3.getSignedUrlPromise("getObject", { Bucket: process.env.LIARA_BUCKET_NAME, Key: `${name}.mp4`, Expires: 60 });
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const s3Delete = async (name) => {
+    try {
+        const s3 = new S3({
+            endpoint: process.env.LIARA_ENDPOINT,
+            accessKeyId: process.env.LIARA_ACCESS_KEY,
+            secretAccessKey: process.env.LIARA_SECRET_KEY,
+            region: "default",
+        });
+
+        await s3.deleteObject({ Bucket: process.env.LIARA_BUCKET_NAME, Key: `${name}.mp4` }).promise();
     } catch (error) {
         throw error;
     }
