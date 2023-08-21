fa272c20e7649ea1dba5f371f4f061e0c506141d
amir25n <amir25nk@gmail.com>
2023-08-21 11:07:20 +0330

    feat(upload/video): create and validate video

--- school-api/routes/videoRoutes.js
+++ school-api/routes/videoRoutes.js
@@ -2,13 +2,15 @@ import { Router } from "express";
 import multer from "multer";
 
 import { s3Upload, s3Download } from "../utils/s3.js";
+import { auth } from "../middlewares/auth.js";
+import { Video } from "../model/videoModel.js";
 
 const router = Router();
 
 const upload = multer({
     storage: multer.memoryStorage(),
     fileFilter: (req, file, cb) => {
-        if (file.mimetype.split("/")[0] === "image") {
+        if (file.mimetype.split("/")[0] === "video") {
             cb(null, true);
         } else {
             cb("not valid", false);
@@ -16,10 +18,22 @@ const upload = multer({
     },
 });
 
-router.post("/upload", upload.any(), async (req, res) => {
-    console.log(req.body.title, req.files);
+// @desc   upload video
+// @route  POST /videos/upload
+// @access private admin
+
+router.post("/upload", auth, upload.any(), async (req, res) => {
     try {
-        await s3Upload(req.files[0]);
+        if (!req.body.studyField || !req.body.grade || !req.body.name || !req.body.title) {
+            return res.json({ message: "input-notvalid" });
+        }
+        await Video.create({
+            studyField: req.body.studyField,
+            grade: req.body.grade,
+            name: req.body.name,
+            title: req.body.title,
+        });
+        await s3Upload(req.files[0], req.body.name);
         res.json({ message: "success" });
     } catch (error) {
         res.json({ message: error });
