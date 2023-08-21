015dd63b906730f36c980bc3ee2b994e2aa7b7d7
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-08-21 12:27:23 +0330

    fix(Admin/Courses): fix AdminCoursesLists table

--- school-ui/src/Components/AdminComponents/AdminCourses/AdmnCoursesLists/AdminCoursesLists.jsx
+++ school-ui/src/Components/AdminComponents/AdminCourses/AdmnCoursesLists/AdminCoursesLists.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import "./AdminCoursesLists.css"
+import axios from 'axios'
+import Cookies from 'js-cookie'
 function AdminCoursesLists() {
+
+
+  useEffect(()=>{
+    axios.get("https://school-node.iran.liara.run/videos/list" , {
+
+      headers:{
+        token : Cookies.get("adminToken")
+      }
+
+    }).then(res => console.log(res))
+
+  },[])
+
   return (
-    <div>AdminCoursesLists</div>
+    <div className='admin-courses-lists left-part'>
+      <h2>فهرست ویدیو ها</h2>
+
+      <table>
+        <thead>
+        <tr>
+
+        <th>تیتر ویدیو</th>
+        <th>پایه تحصیلی ویدیو</th>
+        <th>رشته</th>
+        <th>عملیات</th>
+
+        </tr>
+
+        </thead>
+        <tbody>
+
+        <tr>
+          td
+        </tr>
+
+        </tbody>
+      </table>
+
+    </div>
   )
 }
 
