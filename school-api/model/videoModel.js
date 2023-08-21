49f5a12556ae23510e615af38f353807d9297af3
amir25n <amir25nk@gmail.com>
2023-08-21 09:50:55 +0330

    feat(videos/model): add titile to model

--- school-api/model/videoModel.js
+++ school-api/model/videoModel.js
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 
 const videoSchema = new Schema({
-    feild: {
+    studyField: {
         type: String,
         required: true,
         trim: true,
@@ -15,6 +15,11 @@ const videoSchema = new Schema({
         type: String,
         required: true,
         unique: true,
+        trim: true,
+    },
+    title: {
+        type: String,
+        required: true,
         trim: true,
     },
 });
