f8cd96c0214dae2c51b177cdeb2691ad395a81c9
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-08-21 13:01:05 +0330

    fix(Admin/Courses): Video lists and delete api handeling

--- school-ui/src/Components/AdminComponents/AdminCourses/AdmnCoursesLists/AdminCoursesLists.jsx
+++ school-ui/src/Components/AdminComponents/AdminCourses/AdmnCoursesLists/AdminCoursesLists.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./AdminCoursesLists.css"
 import axios from 'axios'
 import Cookies from 'js-cookie'
+import {AiFillDelete} from "react-icons/ai"
 function AdminCoursesLists() {
 
+  const [allVideos , setAllVideos] = useState([])
 
   useEffect(()=>{
     axios.get("https://school-node.iran.liara.run/videos/list" , {
@@ -12,9 +14,20 @@ function AdminCoursesLists() {
         token : Cookies.get("adminToken")
       }
 
-    }).then(res => console.log(res))
+    }).then(res => setAllVideos(res.data.list))
 
   },[])
+
+
+  const deleteVideo = (videoName) => {
+    axios.delete(`https://school-node.iran.liara.run/videos/delete/${videoName}` , {
+
+      headers : {
+        token : Cookies.get("adminToken")
+      }
+
+    }).then(res => console.log(res))
+  }
 
   return (
     <div className='admin-courses-lists left-part'>
@@ -25,6 +38,7 @@ function AdminCoursesLists() {
         <tr>
 
         <th>تیتر ویدیو</th>
+        <th>نام ویدیو</th>
         <th>پایه تحصیلی ویدیو</th>
         <th>رشته</th>
         <th>عملیات</th>
@@ -34,9 +48,23 @@ function AdminCoursesLists() {
         </thead>
         <tbody>
 
-        <tr>
-          td
-        </tr>
+          {
+            allVideos.map(video => (
+              <tr key={video.id}>
+              
+              
+              <td>{video.title}</td>
+              <td>{video.name}</td>
+              <td>
+                {video.grade === "1" && ("دهم")}
+                {video.grade === "2" && ("یازدهم")}
+                {video.grade === "3" && ("دوازدهم")}
+                </td>
+              <td>{video.studyField === "1" ? "تجربی"  : "ریاضی"}</td>
+              <td><AiFillDelete title='حذف ویدیو' className='opreation-icon delete-icon' onClick={() => deleteVideo(video.name)} /></td>
+              </tr>
+            ))
+          }
 
         </tbody>
       </table>
