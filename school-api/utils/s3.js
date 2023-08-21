c359ed265d506f3ca612f1a9a86ac0a2c0d11ef5
amir25n <amir25nk@gmail.com>
2023-08-21 10:08:30 +0330

    feat(aws/download): get link download with name

--- school-api/utils/s3.js
+++ school-api/utils/s3.js
@@ -1,7 +1,7 @@
 import S3 from "aws-sdk/clients/s3.js";
 import { v4 } from "uuid";
 
-export const s3Upload = async (file) => {
+export const s3Upload = async (file, name) => {
     try {
         const s3 = new S3({
             endpoint: process.env.LIARA_ENDPOINT,
@@ -12,10 +12,25 @@ export const s3Upload = async (file) => {
         return await s3
             .putObject({
                 Bucket: process.env.LIARA_BUCKET_NAME,
-                Key: `${v4()}-${file.originalname}`,
+                Key: name,
                 Body: file.buffer,
             })
             .promise();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const s3Download = async (name) => {
+    try {
+        const s3 = new S3({
+            endpoint: process.env.LIARA_ENDPOINT,
+            accessKeyId: process.env.LIARA_ACCESS_KEY,
+            secretAccessKey: process.env.LIARA_SECRET_KEY,
+            region: "default",
+        });
+        const params = { Bucket: process.env.LIARA_BUCKET_NAME, Key: name, Expires: 60 };
+        return await s3.getSignedUrlPromise("getObject", params);
     } catch (error) {
         console.log(error);
     }
