395692b7a090b36522cf344e3d052b6662890549
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-08-22 17:39:47 +0330

    fix(Students/Courses)

--- school-ui/src/Components/CoursesComponents/CoursesVideo/CoursesVideo.jsx
+++ school-ui/src/Components/CoursesComponents/CoursesVideo/CoursesVideo.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams , Link } from 'react-router-dom'
 import Cookies from "js-cookie"
 import axios from "axios"
+import "./CoursesVideo.css"
+import {MdSchool} from 'react-icons/md'
+import {AiOutlineArrowLeft} from 'react-icons/ai'
+
 
 function CoursesVideo() {
     const [videoInfos , setVideoInfos] = useState([])
@@ -9,19 +13,33 @@ function CoursesVideo() {
 
 
     useEffect(()=>{
+
+
       axios.get(`https://school-node.iran.liara.run/videos/link/${routePath}` , {
         headers :{
           token : Cookies.get("studentToken")
         }
       }).then(res => setVideoInfos(res.data.link))
-    } , [])
 
+
+
+    } , [])
+    
   return (
-    <div>
+    <>
+    
+    <div className='video-container'>
+      <Link className='back-link' to="/student-courses">
+        <AiOutlineArrowLeft />
+      </Link>
+      <h2>تیتر ویدیو</h2>
       <video onContextMenu={(e) => e.preventDefault()} src={videoInfos} controls controlsList="nodownload" className='video'>
         <source src={videoInfos}/>
       </video>
+      <p><MdSchool /> دهم - ریاضی</p>
     </div>
+
+    </>
   )
 }
 
