52e090acad3ff04c802f2d357aa0d5f8ae9812e0
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-08-22 11:43:16 +0330

    feat(Students/Courses): Initial settings of the component

--- school-ui/src/Components/CoursesComponents/CoursesVideosCards/CoursesVideosCards.jsx
+++ school-ui/src/Components/CoursesComponents/CoursesVideosCards/CoursesVideosCards.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./CoursesVideosCards.css"
-import {AiOutlineClockCircle} from 'react-icons/ai'
-function CoursesVideosCards() {
+import { Link } from 'react-router-dom'
+
+function CoursesVideosCards({name, title , grade}) {
+
   return (
+    <Link to={`/student-courses/${name}`}>
+    
     <div className="courses-videos-cards">
         <img src="./images/video-cover.jpg" alt="image cover" className="course-cover" />
         <div className="video-card-body">
-            <h3>تیتر ویدیو</h3>
-            <p>نام دبیر</p>
-        </div>
-        <div className="video-card-footer">
-            <AiOutlineClockCircle />
-            ۱۸/۵/۱۴۰۲
+            <h3>{title}</h3>
+            <p>{grade}</p>
         </div>
     </div>
+    
+    </Link>
   )
 }
 
