7ca685140324fb638a81a3aca839cfe1040e36af
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-08-22 16:25:49 +0330

    fix(Courses/videos) : fix videos logics

--- school-ui/src/Components/CoursesComponents/CoursesVideosCards/CoursesVideosCards.jsx
+++ school-ui/src/Components/CoursesComponents/CoursesVideosCards/CoursesVideosCards.jsx
@@ -2,16 +2,22 @@ import React, { useEffect, useState } from 'react'
 import "./CoursesVideosCards.css"
 import { Link } from 'react-router-dom'
 
-function CoursesVideosCards({name, title , grade}) {
+function CoursesVideosCards({name, title , grade , stydyField}) {
 
   return (
-    <Link to={`/student-courses/video/${name}`}>
+    <Link className='card-links' to={`/student-courses/video/${name}`}>
     
     <div className="courses-videos-cards">
         <img src="./images/video-cover.jpg" alt="image cover" className="course-cover" />
         <div className="video-card-body">
             <h3>{title}</h3>
-            <p>{grade}</p>
+            <p>
+              {grade === "1" && (" دهم ")}
+              {grade === "2" && (" یازدهم ")}
+              {grade === "3" && (" دوازدهم ")}
+              -
+              {stydyField === "1" ? " تجربی " : " ریاضی "}
+            </p>
         </div>
     </div>
     
