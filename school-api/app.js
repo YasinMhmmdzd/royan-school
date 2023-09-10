4757e560bc6dc06782d362e0f23e2bf8b383347d
amir25n <amir25nk@gmail.com>
2023-09-10 12:20:53 +0330

    fix(nginx/reload): bug relode page

--- school-api/app.js
+++ school-api/app.js
@@ -8,9 +8,9 @@ import publicRoutes from "./routes/publicRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import videoRoutes from "./routes/videoRoutes.js";
+import path from "path";
 
 const app = express();
-//* config
 if (process.env.NODE_ENV == "development") {
     dotenv.config({ path: "./config/config.env" });
 }
@@ -26,5 +26,13 @@ app.use(publicRoutes);
 app.use("/admin", adminRoutes);
 app.use("/user", userRoutes);
 app.use("/videos", videoRoutes);
+app.get("/*", (req, res) => {
+    console.log(path.json(path.resolve(), "index.html"));
+    res.sendFile(path.json(path.resolve(), "index.html"), (err) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
+});
 
 app.listen(process.env.PORT, () => console.log(`server is runing on port ${process.env.PORT} and mode ${process.env.NODE_ENV}`));
